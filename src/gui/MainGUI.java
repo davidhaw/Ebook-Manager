029d43413d0507007e0ec61dca418115f99562be
David <davidhaw2005@gmail.com>
2019-01-31 16:24:34 -0600

    Added select folder button

--- src/gui/MainGUI.java
+++ src/gui/MainGUI.java
@@ -1,16 +1,20 @@
 package gui;
 import java.io.File;
 
-import org.zeroturnaround.zip.ZipUtil;
-
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.ComboBoxListCell;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
  
+import org.zeroturnaround.zip.ZipUtil;
+
 @SuppressWarnings("restriction")
 public class MainGUI extends Application {
     public static void main(String[] args) {
@@ -20,6 +24,12 @@ public class MainGUI extends Application {
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Ebook Manager");
+        
+        ListView<String> list = new ListView<String>();
+        ObservableList<String> items =FXCollections.observableArrayList (
+            "Single", "Double", "Suite", "Family App");
+        list.setItems(items);
+
         Button btn = new Button();
         btn.setText("Compress Ebook Folder Into ZIP");
         btn.setOnAction(new EventHandler<ActionEvent>() {
@@ -30,7 +40,9 @@ public class MainGUI extends Application {
     			System.out.println("Ebook Folder has been Exported to Ebooks.zip");            }
         });
         
+        
         StackPane root = new StackPane();
+        root.getChildren().add(list);
         root.getChildren().add(btn);
         primaryStage.setScene(new Scene(root, 500, 450));
         primaryStage.show();
