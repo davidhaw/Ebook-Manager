4e9e1ff2100abb8dedea3d2aeab8b9da038192e6
David <davidhaw2005@gmail.com>
2019-02-01 19:05:52 -0600

    Zipping Files Added
    Button for zipping files now gets the directory from the directory label and then zips the file into ebooks.zip in the event handler.

--- src/gui/MainGUI.java
+++ src/gui/MainGUI.java
@@ -1,50 +1,84 @@
 package gui;
+
+import javafx.scene.control.Label;
 import java.io.File;
 
+import org.zeroturnaround.zip.ZipUtil;
+
+//import org.zeroturnaround.zip.ZipUtil;
+
+
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.control.cell.ComboBoxListCell;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
- 
-import org.zeroturnaround.zip.ZipUtil;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
 
 @SuppressWarnings("restriction")
 public class MainGUI extends Application {
-    public static void main(String[] args) {
-        launch(args);
-    }
+   
     
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Ebook Manager");
-        
-        ListView<String> list = new ListView<String>();
-        ObservableList<String> items =FXCollections.observableArrayList (
-            "Single", "Double", "Suite", "Family App");
-        list.setItems(items);
 
-        Button btn = new Button();
-        btn.setText("Compress Ebook Folder Into ZIP");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-    			ZipUtil.pack(new File("./Ebooks"), new File("./demo.zip"));
-    			System.out.println("Ebook Folder has been Exported to Ebooks.zip");            }
-        });
-        
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(list);
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 500, 450));
-        primaryStage.show();
+    	//Label to show ebook directory. DELETE LATER
+    	final Label labelSelectedDirectory = new Label();
+    	labelSelectedDirectory.setText("./Ebooks");
+    	
+    	//Zip Ebook folder into ebooks.zip
+    	Button zipFolder = new Button();
+    	zipFolder.setText("Zip Ebooks Folder into ebooks.zip");
+    	zipFolder.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				// TODO Put zip code in her that zips to user chosen directory
+				ZipUtil.pack(new File(labelSelectedDirectory.getText()), new File("./Ebooks.zip"));
+				System.out.println("Ebook Folder has been Exported to Ebooks.zip");
+				
+			}
+    		
+    		
+    		
+    	});
+    	
+    	//Get Ebook Folder Button
+    	Button btnOpenDirectoryChooser = new Button();
+    	btnOpenDirectoryChooser.setText("Open DirectoryChooser");
+    	btnOpenDirectoryChooser.setOnAction(new EventHandler<ActionEvent>() {
+    	public void handle(ActionEvent event) {
+    		
+    		DirectoryChooser directoryChooser = new DirectoryChooser();
+    		File selectedDirectory = directoryChooser.showDialog(primaryStage);
+    		if(selectedDirectory == null) {
+    			labelSelectedDirectory.setText("No Directory Selected");
+    		} else {
+    	    	labelSelectedDirectory.setText(selectedDirectory.getAbsolutePath());
+    		}
+    	}
+    	
+    	
+       	});
+    	
+    	VBox vBox = new VBox();
+    	vBox.getChildren().addAll(
+    			btnOpenDirectoryChooser,
+    			zipFolder,
+    			labelSelectedDirectory);
+  
+    	StackPane root = new StackPane();
+    	root.getChildren().add(vBox);
+    	Scene scene = new Scene(root, 500, 450);
+
+    	primaryStage.setTitle("Ebook Manager");
+    	primaryStage.setScene(scene);
+    	primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
     }
 }
